96f4c6d98fd78ea30ae6cae25376a03fbf103576
Chris Tweedie <cltweedie@gmail.com>
2015-04-20 16:39:48 +0100

    Improve efficiency of primes.rb

--- primes.rb
+++ primes.rb
@@ -1,28 +1,34 @@
 class Primes
   def self.sum_to(limit = 100)
     sum = 0
-    for num in 2..limit           # iterate through each number up to the to set limit
-      is_prime = true             # set is_prime as true by default
+    for num in 2..limit                   # iterate through each number up to the to set limit
+      is_prime = true                     # set is_prime as true by default
       puts "trying #{num}..."
-      for divider in 2..num-1     # iterate through each number that it could divide by
-        if num % divider == 0     # if there is no remainder then the number isn't prime
-          is_prime = false        # so set is_prime boolean to false
-          puts "not prime."
-          break                   # and break out of the loop
-        else
-          next                    # otherwise, try the next possible dividing number
+      if num.even? && num != 2            # If the number is even (and not 2), don't bother testing it
+        is_prime = false
+        puts "not prime"
+        next
+      else
+        for divider in 2..Math.sqrt(num)  # iterate through each number that it could divide by
+          if num % divider == 0           # if there is no remainder then the number isn't prime
+            is_prime = false
+            puts "not prime"              # so set is_prime boolean to false
+            break                         # and break out of the loop
+          else
+            next                          # otherwise, try the next possible dividing number
+          end
         end
       end
-      if is_prime == true         # if is_prime is still true after trying every divider number then it's prime
-        sum += num                # so add it to the total
-        puts "is prime."
+      if is_prime == true                 # if is_prime is still true then it's prime
+        sum += num                        # so add it to the total
+        puts num.to_s + " is prime"                           
       end
     end
     puts "The sum of all prime numbers up to #{limit} is #{sum}"                  
   end
 end
 
-Primes.sum_to(10)
+Primes.sum_to(2000000)
 
-# The sum of all prime numbers up to 2000000 is 142913828922
-# It took my PC over 3 hours to figure this out!
+# This was MUCH faster (took a couple of minutes)
+# The answer it gave was 142913828922 - same answer, but much more efficient algorithm.
